cf2b1d3ccbddeee81ca9016d8ea595c4b995b585
Debora Sampaio <deborasampaio73@gmail.com>
2021-10-22 16:13:34 -0300

    alterações no teste automatizado

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,9 +2,17 @@
 
 
 context('Funcionalidade Login', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
 
     it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    
         cy.get('#username').type('testenovo_20@teste.com')
         cy.get('#password').type('@Teste123')
         cy.get('.woocommerce-form > .button').click()
@@ -16,7 +24,6 @@ context('Funcionalidade Login', () => {
     });
 
     it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('teste_20@teste.com')
         cy.get('#password').type('@Teste123')
         cy.get('.woocommerce-form > .button').click()
@@ -27,7 +34,6 @@ context('Funcionalidade Login', () => {
     });
 
     it('Deve exibir uma mensagem de erro ao inserir senha invalida', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('testenovo_20@teste.com')
         cy.get('#password').type('Teste123')
         cy.get('.woocommerce-form > .button').click()
